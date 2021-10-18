57f96a05f943328ea2b24330c32a4391b8df1e5d
Shahriyor <shahriyordeveloping@gmail.com>
2021-10-18 12:00:51 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -14,3 +14,25 @@ const introSlider = new Swiper(".intro", {
         el.style.cssText = `--t: 1; --o: 0; --d: ${d}; --x:${e.clientX - r.left}; --y:${e.clientY - r.top};`
     })
 })
+
+// Header Menu
+const headerButton: HTMLButtonElement =
+  document.querySelector(".header__button");
+const headerMenu: HTMLUListElement = document.querySelector(".header__menu");
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+};
+
+headerButton.onclick = menuToggle;
+
+window.onclick = (e: MouseEvent) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle();
+};
